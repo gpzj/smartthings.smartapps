bee9461a3c714323a951233c1bd3076cee00896e
gpzj <41334202+gpzj@users.noreply.github.com>
2020-05-31 10:06:01 -0400

    Update humidity-on-the-rise.groovy
    Fixed extra } from copy/paste

--- smartapps/gpzj/humidity-on-the-rise.src/humidity-on-the-rise.groovy
+++ smartapps/gpzj/humidity-on-the-rise.src/humidity-on-the-rise.groovy
@@ -24,9 +24,7 @@ preferences {
   }
   section("Alerting") {
      input "phoneNum", "phone", title: "SMS Phone Number.", required: false, multiple: true
-  }
-     input "sendPush", "bool", required: false,
-              title: "Send Push Notification?"
+     input "sendPush", "bool", title: "Send Push Notification?", required: false
   }
 }
 
